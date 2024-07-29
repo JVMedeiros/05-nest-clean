f8434b2b7c37eedb723dcd3787b8a49131642887
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-29 04:03:43 -0300

    feat: implementing create question controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 import { CreateAccountController } from './controllers/create.account.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
@@ -11,7 +12,7 @@ import { PrismaService } from './prisma/prisma.service'
     validate: env => envSchema.parse(env),
     isGlobal: true,
   }), AuthModule],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
   providers: [PrismaService],
 })
 export class AppModule { }
