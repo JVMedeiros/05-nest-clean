995296aa5df7c3455e10aab66ec7dc54fee7c21d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 11:59:58 -0300

    feat: implementing http module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './http/controllers/authenticate.controller'
-import { CreateQuestionController } from './http/controllers/create-question.controller'
-import { CreateAccountController } from './http/controllers/create-account.controller'
-import { FetchRecentQuestionsController } from './http/controllers/fetch-recent-questions.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
+import { HttpModule } from './http/http.module'
 
 @Module({
   imports: [
@@ -15,13 +12,7 @@ import { PrismaService } from './prisma/prisma.service'
       isGlobal: true,
     }),
     AuthModule,
+    HttpModule
   ],
-  controllers: [
-    CreateAccountController,
-    AuthenticateController,
-    CreateQuestionController,
-    FetchRecentQuestionsController,
-  ],
-  providers: [PrismaService],
 })
 export class AppModule { }
