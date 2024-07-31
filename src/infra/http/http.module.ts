c14ec9a830bc1ee0aff228591889e9a985feb586
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 22:51:36 -0300

    feat: implementing useCase layer in infra

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
 import { PrismaService } from '../database/prisma/prisma.service'
+import { NestCreateQuestionUseCase } from '../factories'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
@@ -14,6 +15,6 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
+  providers: [PrismaService, NestCreateQuestionUseCase],
 })
-export class HttpModule {}
+export class HttpModule { }
