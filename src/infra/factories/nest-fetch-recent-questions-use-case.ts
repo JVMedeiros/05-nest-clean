c8049ad1e057d8b0496e62093a0280b648c5999d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 23:10:37 -0300

    fix: eslint fix

--- src/infra/factories/nest-fetch-recent-questions-use-case.ts
+++ src/infra/factories/nest-fetch-recent-questions-use-case.ts
@@ -1,8 +1,6 @@
-import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-respository";
-import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
-import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
-import { Injectable } from "@nestjs/common";
-
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-respository'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class NestFetchQuestionsUseCase extends FetchRecentQuestionsUseCase {
