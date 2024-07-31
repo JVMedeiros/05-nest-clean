b737eebd4cb86a9e52be84bdc3fa3298dd3c79e0
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 23:10:48 -0300

    fix: eslint fix

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
 import { CurrentUser } from '../../auth/current-user-decorator'
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
 import { UserPayload } from '../../auth/jwt.strategy'
@@ -18,9 +17,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(
-    private createQuestion: NestCreateQuestionUseCase
-  ) { }
+  constructor(private createQuestion: NestCreateQuestionUseCase) {}
 
   @Post()
   async handle(
@@ -29,7 +26,6 @@ export class CreateQuestionController {
   ) {
     const { title, content } = body
     const userId = user.sub
-    const slug = this.convertToSlug(title)
 
     await this.createQuestion.execute({
       title,
@@ -37,14 +33,5 @@ export class CreateQuestionController {
       authorId: userId,
       attachmentsIds: [],
     })
-  }
-
-  private convertToSlug(title: string): string {
-    return title
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/[^\w\s-]/g, '')
-      .replace(/\s+/g, '-')
   }
 }
