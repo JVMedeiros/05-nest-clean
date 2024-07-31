e2d639223827dc7217497c71a22284187de61d20
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-31 13:06:30 -0300

    test: register student

---
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -0,0 +1,60 @@
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { expect } from 'vitest'
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
+import { RegisterStudentUseCase } from './register-student'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let sut: RegisterStudentUseCase
+
+describe('Register Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+    sut = new RegisterStudentUseCase(inMemoryStudentsRepository, fakeHasher)
+  })
+
+  it('Should be able to register a student', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      student: inMemoryStudentsRepository.items[0]
+    })
+  })
+
+  it('Should hash student password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword)
+  })
+
+  it('Should not be able to register a student with duplicated email', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(StudentAlreadyExistsError)
+  })
+})
