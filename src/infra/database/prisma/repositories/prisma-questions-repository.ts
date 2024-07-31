258003f4f2a2fb22ad67242c123c61759faf0eff
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 22:50:31 -0300

    fix: eslint fix

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -7,13 +7,13 @@ import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
   async create(question: Question): Promise<void> {
     const data = PrismaQuestionMapper.toPersistance(question)
 
     await this.prisma.question.create({
-      data
+      data,
     })
   }
 
@@ -22,8 +22,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
 
     await this.prisma.question.delete({
       where: {
-        id: data.id
-      }
+        id: data.id,
+      },
     })
   }
 
@@ -32,17 +32,17 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
 
     await this.prisma.question.update({
       where: {
-        id: data.id
+        id: data.id,
       },
-      data
+      data,
     })
   }
 
   async findById(id: string): Promise<Question | null> {
     const question = await this.prisma.question.findUnique({
       where: {
-        id
-      }
+        id,
+      },
     })
 
     if (!question) {
@@ -55,8 +55,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   async findBySlug(slug: string): Promise<Question | null> {
     const question = await this.prisma.question.findUnique({
       where: {
-        slug
-      }
+        slug,
+      },
     })
 
     if (!question) {
@@ -69,10 +69,10 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
     const questions = await this.prisma.question.findMany({
       orderBy: {
-        createdAt: 'desc'
+        createdAt: 'desc',
       },
       take: 20,
-      skip: (page - 1) * 20
+      skip: (page - 1) * 20,
     })
 
     return questions.map(PrismaQuestionMapper.toDomain)
