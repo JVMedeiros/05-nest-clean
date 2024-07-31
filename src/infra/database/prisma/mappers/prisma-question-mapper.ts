ab5480d2fe272f6d935b5dd1d39e5e36cf8f40b4
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 22:50:12 -0300

    feat: add toPersistance method to mapper

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,25 +1,29 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Question } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Prisma, Question as PrismaQuestion } from "@prisma/client";
-import { title } from "process";
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Prisma, Question as PrismaQuestion } from '@prisma/client'
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
-    return Question.create({
-      title: raw.title,
-      content: raw.content,
-      authorId: new UniqueEntityID(raw.authorId),
-      bestAnswerId: raw.bestAnswerId
-        ? new UniqueEntityID(raw.bestAnswerId)
-        : null,
-      slug: Slug.create(raw.slug),
-      createdAt: raw.createdAt,
-      updatedAt: raw.updatedAt
-    }, new UniqueEntityID(raw.id))
+    return Question.create(
+      {
+        title: raw.title,
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        bestAnswerId: raw.bestAnswerId
+          ? new UniqueEntityID(raw.bestAnswerId)
+          : null,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    )
   }
 
-  static toPersistance(question: Question): Prisma.QuestionUncheckedCreateInput {
+  static toPersistance(
+    question: Question,
+  ): Prisma.QuestionUncheckedCreateInput {
     return {
       id: question.id.toString(),
       authorId: question.authorId.toString(),
@@ -28,7 +32,7 @@ export class PrismaQuestionMapper {
       content: question.content,
       slug: question.slug.value,
       createdAt: question.createdAt,
-      updatedAt: question.updatedAt
+      updatedAt: question.updatedAt,
     }
   }
 }
