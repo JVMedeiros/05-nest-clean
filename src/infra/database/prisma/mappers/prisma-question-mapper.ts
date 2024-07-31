8a5d2f99afca04b8f856e0b68167d4b4b14750ea
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 22:27:50 -0300

    feat: create toPersistance mapper method

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,8 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Question as PrismaQuestion } from "@prisma/client";
+import { Prisma, Question as PrismaQuestion } from "@prisma/client";
+import { title } from "process";
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
@@ -16,5 +17,18 @@ export class PrismaQuestionMapper {
       createdAt: raw.createdAt,
       updatedAt: raw.updatedAt
     }, new UniqueEntityID(raw.id))
+  }
+
+  static toPersistance(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt
+    }
   }
 }
