5b4936a59e0727ead8704b6901c7764fa7c3c265
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-06 00:13:13 -0300

    feat: prisma students mapper

---
+++ src/infra/database/prisma/mappers/prisma-student-mapper.ts
@@ -0,0 +1,28 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Student } from '@/domain/forum/enterprise/entities/student'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Prisma, User as PrismaUser } from '@prisma/client'
+
+export class PrismaStudentMapper {
+  static toDomain(raw: PrismaUser): Student {
+    return Student.create(
+      {
+        name: raw.name,
+        email: raw.email,
+        password: raw.password,
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+
+  static toPersistance(
+    student: Student,
+  ): Prisma.UserUncheckedCreateInput {
+    return {
+      id: student.id.toString(),
+      name: student.name,
+      email: student.email,
+      password: student.password,
+    }
+  }
+}
