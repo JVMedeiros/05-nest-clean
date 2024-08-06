bf5a86fee654dd58b7d59967cc380ecc998f885c
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-06 17:26:40 -0300

    feat: tratative for controller errors

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -2,11 +2,14 @@ import {
   Body,
   Controller,
   Post, HttpCode,
-  UsePipes
+  UsePipes,
+  ConflictException,
+  BadRequestException
 } from '@nestjs/common'
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { NestRegisterStudentUseCase } from '@/infra/factories'
+import { StudentAlreadyExistsError } from '@/domain/forum/application/use-cases/errors/student-already-exists-error'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -33,7 +36,14 @@ export class CreateAccountController {
     })
 
     if (result.isLeft()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case StudentAlreadyExistsError:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
   }
 }
