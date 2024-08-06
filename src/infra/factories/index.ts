4216bf033015f9f2c93a0a9d9e48f4fe7bc2a99b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-06 00:36:18 -0300

    feat: add nest register student use case to export file

--- src/infra/factories/index.ts
+++ src/infra/factories/index.ts
@@ -1,3 +1,4 @@
 export * from './nest-create-question-use-case'
 export * from './nest-fetch-recent-questions-use-case'
 export * from './nest-authenticate-student-use-case'
+export * from './nest-register-student-use-case'
