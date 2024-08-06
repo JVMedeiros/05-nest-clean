3de1ecfce25db6299f3bd86848b94ce810586252
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-06 00:24:27 -0300

    fix: lint fix

--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,30 +1,25 @@
-import { Encrypter } from "@/domain/forum/application/cryptography/encrypter";
-import { HashComparer } from "@/domain/forum/application/cryptography/hash-comparer";
-import { HashGenerator } from "@/domain/forum/application/cryptography/hash-generator";
-import { Module } from "@nestjs/common";
-import { BcryptHasher } from "./bcrypt-hasher";
-import { JwtEncrypter } from "./jwt-encrypter";
-
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { Module } from '@nestjs/common'
+import { BcryptHasher } from './bcrypt-hasher'
+import { JwtEncrypter } from './jwt-encrypter'
 
 @Module({
   providers: [
     {
       provide: Encrypter,
-      useClass: JwtEncrypter
+      useClass: JwtEncrypter,
     },
     {
       provide: HashComparer,
-      useClass: BcryptHasher
+      useClass: BcryptHasher,
     },
     {
       provide: HashGenerator,
-      useClass: BcryptHasher
-    }
+      useClass: BcryptHasher,
+    },
   ],
-  exports: [
-    Encrypter,
-    HashComparer,
-    HashGenerator
-  ]
+  exports: [Encrypter, HashComparer, HashGenerator],
 })
-export class CryptographyModule { }
+export class CryptographyModule {}
