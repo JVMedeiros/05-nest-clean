1d430d21f666a78a22f823ed6aa0fe7dc85fc547
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-06 00:24:16 -0300

    fix: lint fix

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -1,5 +1,5 @@
-import { HashComparer } from "@/domain/forum/application/cryptography/hash-comparer";
-import { HashGenerator } from "@/domain/forum/application/cryptography/hash-generator";
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
 import { hash, compare } from 'bcryptjs'
 
 export class BcryptHasher implements HashGenerator, HashComparer {
@@ -8,8 +8,8 @@ export class BcryptHasher implements HashGenerator, HashComparer {
   hash(plain: string): Promise<string> {
     return hash(plain, this.HASH_SALT_LENGTH)
   }
+
   compare(plain: string, hash: string): Promise<boolean> {
     return compare(plain, hash)
   }
-
 }
