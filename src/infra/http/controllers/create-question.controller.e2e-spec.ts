152938d9f4c48a06c46e71bf3a2de47e86f74d91
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-10 03:44:47 -0300

    refactor: implementing makeQuestion and makeStudent factory

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -6,35 +6,38 @@ import request from 'supertest'
 import { hash } from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt'
 import { makeRandomString } from 'test/factories/make-random-string'
+import { StudentFactory } from 'test/factories/make-student'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 describe('Create Question (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
+  let studentFactory: StudentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory]
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
   test('[POST] /questions', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: makeRandomString(),
-        email: makeRandomString(),
-        password: await hash(makeRandomString(), 8),
-      },
+    const user = await studentFactory.makePrismaStudent({
+      name: makeRandomString(),
+      email: makeRandomString(),
+      password: await hash(makeRandomString(), 8),
     })
 
-    const accessToken = jwt.sign({ sub: user.id })
+    const accessToken = jwt.sign({ sub: user.id.toString() })
     const fakeQuestion = {
       title: makeRandomString(),
       content: makeRandomString(),
