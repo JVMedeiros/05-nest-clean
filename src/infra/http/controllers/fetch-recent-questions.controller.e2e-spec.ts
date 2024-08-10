9a6d12b23e6477f9029435cb3da136c055557232
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-10 03:44:51 -0300

    refactor: implementing makeQuestion and makeStudent factory

--- src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -6,60 +6,55 @@ import request from 'supertest'
 import { hash } from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt'
 import { makeRandomString } from 'test/factories/make-random-string'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 describe('Fetch Recent Questions (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory]
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
   test('[GET] /questions', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: makeRandomString(),
-        email: makeRandomString(),
-        password: await hash('123456', 8),
-      },
-    })
+    const user = await studentFactory.makePrismaStudent()
 
-    const accessToken = jwt.sign({ sub: user.id })
+    const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    await prisma.question.createMany({
-      data: [
-        {
-          title: makeRandomString(),
-          slug: makeRandomString(),
-          content: makeRandomString(),
-          authorId: user.id,
-        },
-        {
-          title: makeRandomString(),
-          slug: makeRandomString(),
-          content: makeRandomString(),
-          authorId: user.id,
-        },
-        {
-          title: makeRandomString(),
-          slug: makeRandomString(),
-          content: makeRandomString(),
-          authorId: user.id,
-        },
-      ],
-    })
+    await Promise.all([
+      questionFactory.makePrismaQuestion({
+        authorId: user.id,
+        title: makeRandomString(),
+      }),
+      questionFactory.makePrismaQuestion({
+        authorId: user.id,
+        title: makeRandomString(),
+      }),
+      questionFactory.makePrismaQuestion({
+        authorId: user.id,
+        title: makeRandomString(),
+      }),
+    ])
 
     const fakeQuestions = await prisma.question.findMany()
+
     const response = await request(app.getHttpServer())
       .get('/questions')
       .set('Authorization', `Bearer ${accessToken}`)
