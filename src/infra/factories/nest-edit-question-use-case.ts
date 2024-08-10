34c238b4b400c1a30a9d700e6940cbd891e7c57d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-10 12:38:02 -0300

    feat: edit question use case implementation

---
+++ src/infra/factories/nest-edit-question-use-case.ts
@@ -0,0 +1,17 @@
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-respository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class NestEditQuestionUseCase extends EditQuestionUseCase {
+  constructor(
+    questionsRepository: QuestionsRepository,
+    questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {
+    super(questionsRepository, questionAttachmentsRepository)
+  }
+}
