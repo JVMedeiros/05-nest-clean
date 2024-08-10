963a8e7e42fdbc111107bbf199fd93adf5c05605
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-10 02:40:02 -0300

    refactor: implementing randomString and randomEmail factory

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -5,6 +5,7 @@ import { Test } from '@nestjs/testing'
 import request from 'supertest'
 import { hash } from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt'
+import { makeRandomString } from 'test/factories/make-random-string'
 
 describe('Create Question (E2E)', () => {
   let app: INestApplication
@@ -27,27 +28,31 @@ describe('Create Question (E2E)', () => {
   test('[POST] /questions', async () => {
     const user = await prisma.user.create({
       data: {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: await hash('123456', 8),
+        name: makeRandomString(),
+        email: makeRandomString(),
+        password: await hash(makeRandomString(), 8),
       },
     })
 
     const accessToken = jwt.sign({ sub: user.id })
+    const fakeQuestion = {
+      title: makeRandomString(),
+      content: makeRandomString(),
+    }
 
     const response = await request(app.getHttpServer())
       .post('/questions')
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
-        title: 'New Question',
-        content: 'Question Content',
+        title: fakeQuestion.title,
+        content: fakeQuestion.content,
       })
 
     expect(response.statusCode).toBe(201)
 
     const questionOnDatabase = await prisma.question.findFirst({
       where: {
-        title: 'New Question',
+        title: fakeQuestion.title,
       },
     })
 
