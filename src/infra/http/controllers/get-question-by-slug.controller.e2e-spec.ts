79d80ecf19dc897df565cadc21868135154656d6
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-10 03:08:37 -0300

    refactor: implementing makeQuestion and makeStudent factory

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -6,43 +6,43 @@ import request from 'supertest'
 import { hash } from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt'
 import { makeRandomString } from 'test/factories/make-random-string'
+import { StudentFactory } from 'test/factories/make-student'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { QuestionFactory } from 'test/factories/make-question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
 describe('Get Question By Slug (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory]
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
   test('[GET] /questions/:slug', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: await hash('123456', 8),
-      },
-    })
+    const fakeUser = await studentFactory.makePrismaStudent()
 
-    const accessToken = jwt.sign({ sub: user.id })
+    const accessToken = jwt.sign({ sub: fakeUser.id.toString() })
 
-    const fakeQuestion = await prisma.question.create({
-      data: {
-        title: makeRandomString(),
-        slug: makeRandomString(),
-        content: makeRandomString(),
-        authorId: user.id,
-      },
+    const fakeQuestion = await questionFactory.makePrismaQuestion({
+      title: makeRandomString(),
+      slug: Slug.create(makeRandomString()),
+      authorId: fakeUser.id,
     })
 
     const response = await request(app.getHttpServer())
