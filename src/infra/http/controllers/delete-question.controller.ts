74407360551959adca1fbbd8a9a387ac844319a4
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-11 12:11:57 -0300

    feat: delete question controller

---
+++ src/infra/http/controllers/delete-question.controller.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Body, Controller, Delete, HttpCode, Param, Put } from '@nestjs/common'
+import { CurrentUser } from '../../auth/current-user-decorator'
+import { UserPayload } from '../../auth/jwt.strategy'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { NestDeleteQuestionUseCase } from '../../factories'
+import { z } from 'zod'
+
+@Controller('/questions/:id')
+export class DeleteQuestionController {
+  constructor(private deleteQuestion: NestDeleteQuestionUseCase) { }
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') questionId: string,
+  ) {
+    const userId = user.sub
+
+    const result = await this.deleteQuestion.execute({
+      questionId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
