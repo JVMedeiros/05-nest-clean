1c509c8e35e4d0e8d6dd4a73da0ed65c8b33f681
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-12 16:18:40 -0300

    feat: fetch question answers controller

---
+++ src/infra/http/controllers/fetch-question-answers.controller.ts
@@ -0,0 +1,39 @@
+import { BadRequestException, Controller, Get, Param, Query } from '@nestjs/common'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { NestFetchQuestionAnswersUseCase, NestFetchQuestionsUseCase } from '@/infra/factories'
+import { QuestionPresenter } from '@/infra/presenters/question-presenter'
+import { AnswerPresenter } from '@/infra/presenters/answer-presenter'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/questions/:questionId/answers')
+export class FetchQuestionAnswerController {
+  constructor(private fetchQuestionAnswers: NestFetchQuestionAnswersUseCase) { }
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('questionId') questionId: string
+  ) {
+    const result = await this.fetchQuestionAnswers.execute({
+      page,
+      questionId
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const answers = result.value.answers
+    return { answers: answers.map(AnswerPresenter.toHTTP) }
+  }
+}
