14aabae3b16d5257b64a9d42cb242d2557981fe5
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-14 00:00:45 -0300

    feat: add comment on question controller

---
+++ src/infra/http/controllers/comment-on-question.controller.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, Body, Controller, Param, Post } from '@nestjs/common'
+import { CurrentUser } from '../../auth/current-user-decorator'
+import { UserPayload } from '../../auth/jwt.strategy'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { NestCommentOnQuestionUseCase } from '../../factories'
+import { z } from 'zod'
+
+const commentOnQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(commentOnQuestionBodySchema)
+type CommentOnQuestionBodySchema = z.infer<typeof commentOnQuestionBodySchema>
+
+@Controller('/questions/:questionId/comments')
+export class CommentOnQuestionController {
+  constructor(private commentOnQuestion: NestCommentOnQuestionUseCase) { }
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CommentOnQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string
+  ) {
+    const { content } = body
+    const userId = user.sub
+
+    const result = await this.commentOnQuestion.execute({
+      content,
+      questionId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
