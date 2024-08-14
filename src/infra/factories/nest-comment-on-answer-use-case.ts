b0059b646fe2e9778cd784f4985ad16bcc6d2d27
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-14 00:19:58 -0300

    feat: add comment on answer use case

---
+++ src/infra/factories/nest-comment-on-answer-use-case.ts
@@ -0,0 +1,14 @@
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-respository'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class NestCommentOnAnswerUseCase extends CommentOnAnswerUseCase {
+  constructor(answersRepository: AnswersRepository, answerCommentsRepository: AnswerCommentsRepository) {
+    super(answersRepository, answerCommentsRepository)
+  }
+}
