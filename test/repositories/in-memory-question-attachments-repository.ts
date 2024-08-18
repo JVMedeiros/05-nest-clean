9025ea563343e3c8cba45dadae8db12654db78f5
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 16:04:25 -0300

    feat: add create many and delete many methods

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -1,5 +1,5 @@
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
-import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attatchment";
+import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
 
 export class InMemoryQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
   public items: QuestionAttachment[] = []
@@ -13,6 +13,18 @@ export class InMemoryQuestionAttachmentsRepository implements QuestionAttachment
 
   async deleteManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter((item) => item.questionId.toString() !== questionId)
+
+    this.items = questionAttachments
+  }
+
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
 
     this.items = questionAttachments
   }
