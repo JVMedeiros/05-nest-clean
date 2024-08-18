155b972706fd75c086d0bd3e5951810cd42a971a
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 17:48:43 -0300

    fix: typo fix

--- src/infra/factories/nest-get-question-by-slug-use-case.ts
+++ src/infra/factories/nest-get-question-by-slug-use-case.ts
@@ -1,4 +1,4 @@
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-respository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { Injectable } from '@nestjs/common'
 
