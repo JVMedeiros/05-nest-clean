4924e9a35f8e7529a7669a21c20797ec1b6b3361
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 19:26:09 -0300

    test: testing attachments scenario

--- src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
@@ -10,19 +10,23 @@ import { StudentFactory } from 'test/factories/make-student'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { QuestionFactory } from 'test/factories/make-question'
 import { AnswerFactory } from 'test/factories/make-answer'
+import { AnswerAttachmentFactory } from 'test/factories/make-answer-attachment'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 
 describe('Edit answer (E2E)', () => {
   let app: INestApplication
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
   let answerFactory: AnswerFactory
+  let attachmentFactory: AttachmentFactory
+  let answerAttachmentFactory: AnswerAttachmentFactory
   let prisma: PrismaService
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory, AnswerFactory]
+      providers: [StudentFactory, QuestionFactory, AnswerFactory, AttachmentFactory, AnswerAttachmentFactory]
     }).compile()
 
     app = moduleRef.createNestApplication()
@@ -31,6 +35,8 @@ describe('Edit answer (E2E)', () => {
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
     answerFactory = moduleRef.get(AnswerFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
+    answerAttachmentFactory = moduleRef.get(AnswerAttachmentFactory)
 
     jwt = moduleRef.get(JwtService)
 
@@ -57,22 +63,55 @@ describe('Edit answer (E2E)', () => {
     const fakePayload = {
       content: makeRandomString(),
     }
+    const fakeAttachment1 = await attachmentFactory.makePrismaAttachment()
+    const fakeAttachment2 = await attachmentFactory.makePrismaAttachment()
+
+    await answerAttachmentFactory.makePrismaAnswerAttachment({
+      attachmentId: fakeAttachment1.id,
+      answerId: fakeAnswer.id
+    })
+    await answerAttachmentFactory.makePrismaAnswerAttachment({
+      attachmentId: fakeAttachment2.id,
+      answerId: fakeAnswer.id
+    })
+
+    const fakeAttachment3 = await attachmentFactory.makePrismaAttachment()
+
 
     const response = await request(app.getHttpServer())
       .put(`/answers/${answerId}`)
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
         content: fakePayload.content,
+        attachments: [
+          fakeAttachment1.id.toString(),
+          fakeAttachment3.id.toString()
+        ]
       })
-
-    expect(response.statusCode).toBe(204)
 
     const answerOnDatabase = await prisma.answer.findFirst({
       where: {
         content: fakePayload.content,
       },
     })
+    const attachmentsOnDatabase = await prisma.attachment.findMany({
+      where: {
+        answerId: answerOnDatabase?.id
+      }
+    })
 
+    expect(response.statusCode).toBe(204)
     expect(answerOnDatabase).toBeTruthy()
+    expect(attachmentsOnDatabase).toHaveLength(2)
+    expect(attachmentsOnDatabase).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: fakeAttachment1.id.toString()
+        }),
+        expect.objectContaining({
+          id: fakeAttachment3.id.toString()
+        }),
+      ])
+    )
   })
 })
