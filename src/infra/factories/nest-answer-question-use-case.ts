40923f6f60488a4d76ca660c4775508e3ab63eea
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 17:47:55 -0300

    fix: typo fix

--- src/infra/factories/nest-answer-question-use-case.ts
+++ src/infra/factories/nest-answer-question-use-case.ts
@@ -1,5 +1,5 @@
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-respository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { Injectable } from '@nestjs/common'
