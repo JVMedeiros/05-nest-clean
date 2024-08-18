5a3a486a5863c13443de1b4adf369c8415ed517f
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 17:48:07 -0300

    fix: typo fix

--- src/infra/factories/nest-comment-on-answer-use-case.ts
+++ src/infra/factories/nest-comment-on-answer-use-case.ts
@@ -1,7 +1,7 @@
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-respository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
 import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
 import { Injectable } from '@nestjs/common'
