b1d91b0a25363e555800a343cfeed168ff234dcb
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 19:24:52 -0300

    feat: add create many and delete many methods

--- src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -9,6 +9,33 @@ export class PrismaAnswerAttachmentsRepository
   implements AnswerAttachmentsRepository {
   constructor(private prisma: PrismaService) { }
 
+  async createMany(attachments: AnswerAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+    const data = PrismaAnswerAttachmentMapper.toPersistanceUpdateMany(attachments)
+
+    await this.prisma.attachment.updateMany(data)
+  }
+
+  async deleteMany(attachments: AnswerAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const attachmentIds = attachments.map(attachment => {
+      return attachment.id.toString()
+    })
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentIds
+        }
+      }
+    })
+  }
+
   async deleteManyByAnswerId(answerId: string): Promise<void> {
     await this.prisma.attachment.deleteMany({
       where: {
