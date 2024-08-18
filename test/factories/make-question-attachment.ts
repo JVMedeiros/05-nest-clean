715ca845d9220115d0c11d9825ae96350033cda9
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 18:53:48 -0300

    feat: add makeQuestionAttachment

--- test/factories/make-question-attachment.ts
+++ test/factories/make-question-attachment.ts
@@ -1,5 +1,8 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { QuestionAttachment, QuestionAttachmentProps } from '@/domain/forum/enterprise/entities/question-attachment';
+import { PrismaQuestionAttachmentMapper } from "@/infra/database/prisma/mappers/prisma-question-attachment-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
 
 export function makeQuestionAttachment(
   override: Partial<QuestionAttachmentProps> = {},
@@ -13,4 +16,24 @@ export function makeQuestionAttachment(
   )
 
   return questionAttachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) { }
+
+  async makePrismaQuestionAttachment(data: Partial<QuestionAttachmentProps> = {}): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString()
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString()
+      }
+    })
+
+    return questionAttachment
+  }
 }
