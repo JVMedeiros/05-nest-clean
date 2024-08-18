7be0f3e5a6946d0c42c7f9bb007d9a4019ab45ac
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 08:09:03 -0300

    fix: nomenclature

--- src/domain/forum/enterprise/entities/attachment.ts
+++ src/domain/forum/enterprise/entities/attachment.ts
@@ -1,23 +1,23 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface AttatchmentProps {
+interface AttachmentProps {
   title: string
-  link: string
+  url: string
 }
 
-export class Attatchment extends Entity<AttatchmentProps> {
+export class Attachment extends Entity<AttachmentProps> {
   get title() {
     return this.props.title
   }
 
-  get link() {
-    return this.props.link
+  get url() {
+    return this.props.url
   }
 
-  static create(props: AttatchmentProps, id?: UniqueEntityID) {
-    const attatchment = new Attatchment(props, id)
+  static create(props: AttachmentProps, id?: UniqueEntityID) {
+    const attachment = new Attachment(props, id)
 
-    return attatchment
+    return attachment
   }
 }
