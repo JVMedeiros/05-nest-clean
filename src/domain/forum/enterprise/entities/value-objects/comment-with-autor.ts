f76770a6d9b49ba69116bc75541f02a1b65359cc
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-18 23:18:40 -0300

    wip: value objects for author

--- src/domain/forum/enterprise/entities/value-objects/comment-with-autor.ts
+++ src/domain/forum/enterprise/entities/value-objects/comment-with-autor.ts
@@ -1,38 +1,37 @@
-import { ValueObject } from "@/core/entities/value-object";
+import { ValueObject } from '@/core/entities/value-object'
 
 export interface CommentWithAuthorProps {
   commentId: string
-  content: string;
+  content: string
   authorId: string
   author: string
   createdAt: Date
-  updatedAt: Date | null
+  updatedAt?: Date | null
 }
-
 
 export class CommentWithAuthor extends ValueObject<CommentWithAuthorProps> {
   get commentId() {
-    return this.commentId
+    return this.props.commentId
   }
 
   get content() {
-    return this.content
+    return this.props.content
   }
 
   get authorId() {
-    return this.authorId
+    return this.props.authorId
   }
 
   get author() {
-    return this.author
+    return this.props.author
   }
 
   get createdAt() {
-    return this.createdAt
+    return this.props.createdAt
   }
 
   get updatedAt() {
-    return this.updatedAt
+    return this.props.updatedAt
   }
 
   static create(props: CommentWithAuthorProps) {
