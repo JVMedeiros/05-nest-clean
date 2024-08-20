420219c44b9790b4acf5655a74ed6f0a0cbdbf82
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:44:45 -0300

    fix: eslint fix

--- src/infra/factories/nest-edit-question-use-case.ts
+++ src/infra/factories/nest-edit-question-use-case.ts
@@ -1,9 +1,6 @@
-import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
-import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
-import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
