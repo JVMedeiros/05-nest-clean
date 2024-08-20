3a29834eeb5bec4a91db4990f0593bd863005e8b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:48:50 -0300

    fix: eslint fix

--- src/infra/http/controllers/get-question-by-slug.controller.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -1,22 +1,10 @@
 import { BadRequestException, Controller, Get, Param } from '@nestjs/common'
-import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
-import { z } from 'zod'
 import { NestGetQuestionBySlugUseCase } from '@/infra/factories'
 import { QuestionPresenter } from '@/infra/presenters/question-presenter'
 
-const pageQueryParamSchema = z
-  .string()
-  .optional()
-  .default('1')
-  .transform(Number)
-  .pipe(z.number().min(1))
-
-const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
-type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
-
 @Controller('/questions')
 export class GetQuestionBySlugController {
-  constructor(private getQuestionBySlug: NestGetQuestionBySlugUseCase) { }
+  constructor(private getQuestionBySlug: NestGetQuestionBySlugUseCase) {}
 
   @Get()
   async handle(@Param('slug') slug: string) {
