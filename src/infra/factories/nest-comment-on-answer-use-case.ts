2d2c33b74b898b95001f364cc3316eb1f103f24c
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:44:16 -0300

    fix: eslint fix

--- src/infra/factories/nest-comment-on-answer-use-case.ts
+++ src/infra/factories/nest-comment-on-answer-use-case.ts
@@ -1,14 +1,14 @@
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
-import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
-import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class NestCommentOnAnswerUseCase extends CommentOnAnswerUseCase {
-  constructor(answersRepository: AnswersRepository, answerCommentsRepository: AnswerCommentsRepository) {
+  constructor(
+    answersRepository: AnswersRepository,
+    answerCommentsRepository: AnswerCommentsRepository,
+  ) {
     super(answersRepository, answerCommentsRepository)
   }
 }
