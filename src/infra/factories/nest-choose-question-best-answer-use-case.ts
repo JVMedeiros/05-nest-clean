1a03835f306088ecdee93f4a7996802c4a70d6e3
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:44:22 -0300

    fix: eslint fix

--- src/infra/factories/nest-choose-question-best-answer-use-case.ts
+++ src/infra/factories/nest-choose-question-best-answer-use-case.ts
@@ -1,9 +1,5 @@
-import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
-import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
-import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
-import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
 import { Injectable } from '@nestjs/common'
 
@@ -11,7 +7,7 @@ import { Injectable } from '@nestjs/common'
 export class NestChooseQuestionBestAnswerUseCase extends ChooseQuestionBestAnswerUseCase {
   constructor(
     questionsRepository: QuestionsRepository,
-    answersRepository: AnswersRepository
+    answersRepository: AnswersRepository,
   ) {
     super(questionsRepository, answersRepository)
   }
