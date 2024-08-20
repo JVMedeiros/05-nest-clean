0b1d7b6cb766ac6a2d28ef4232ecc8753b7ccf16
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:44:48 -0300

    fix: eslint fix

--- src/infra/factories/nest-fetch-answer-comment-use-case.ts
+++ src/infra/factories/nest-fetch-answer-comment-use-case.ts
@@ -1,5 +1,4 @@
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
-import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
 import { Injectable } from '@nestjs/common'
 
