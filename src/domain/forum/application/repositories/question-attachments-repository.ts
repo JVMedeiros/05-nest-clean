6b940d24315a1a5cc1055167aab816615091bc68
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:36:21 -0300

    fix: eslint fix

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -2,7 +2,10 @@ import { QuestionAttachment } from '../../enterprise/entities/question-attachmen
 
 export abstract class QuestionAttachmentsRepository {
   abstract deleteManyByQuestionId(questionId: string): Promise<void>
-  abstract findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
+  abstract findManyByQuestionId(
+    questionId: string,
+  ): Promise<QuestionAttachment[]>
+
   abstract createMany(attachments: QuestionAttachment[]): Promise<void>
   abstract deleteMany(attachments: QuestionAttachment[]): Promise<void>
 }
