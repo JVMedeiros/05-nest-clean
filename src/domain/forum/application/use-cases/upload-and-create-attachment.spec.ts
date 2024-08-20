fc4b87eeafacb080971fef225b988f6c187fa639
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:39:10 -0300

    fix: eslint fix

--- src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
@@ -1,12 +1,7 @@
-import { FakeHasher } from 'test/cryptography/fake-hasher'
 import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
-import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
 import { FakeUploader } from 'test/storage/fake-uploader'
 import { expect } from 'vitest'
-import { Uploader } from '../storage/uploader'
 import { InvalidAttachmentTypeError } from './errors/invalid-attachment-type'
-import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
-import { RegisterStudentUseCase } from './register-student'
 import { UploadAndCreateAttachmentUseCase } from './upload-and-create-attachment'
 
 let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
@@ -17,14 +12,17 @@ describe('Upload and create attachment', () => {
   beforeEach(() => {
     inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
     fakeUploader = new FakeUploader()
-    sut = new UploadAndCreateAttachmentUseCase(inMemoryAttachmentsRepository, fakeUploader)
+    sut = new UploadAndCreateAttachmentUseCase(
+      inMemoryAttachmentsRepository,
+      fakeUploader,
+    )
   })
 
   it('Should be upload and create an attachment', async () => {
     const result = await sut.execute({
       fileName: 'profile.png',
       fileType: 'image/png',
-      body: Buffer.from('')
+      body: Buffer.from(''),
     })
 
     expect(result.isRight()).toBe(true)
@@ -34,8 +32,8 @@ describe('Upload and create attachment', () => {
     expect(fakeUploader.uploads).toHaveLength(1)
     expect(fakeUploader.uploads[0]).toEqual(
       expect.objectContaining({
-        fileName: 'profile.png'
-      })
+        fileName: 'profile.png',
+      }),
     )
   })
 
@@ -43,7 +41,7 @@ describe('Upload and create attachment', () => {
     const result = await sut.execute({
       fileName: 'profile.mp3',
       fileType: 'audio/mpeg',
-      body: Buffer.from('')
+      body: Buffer.from(''),
     })
 
     expect(result.isLeft()).toBe(true)
