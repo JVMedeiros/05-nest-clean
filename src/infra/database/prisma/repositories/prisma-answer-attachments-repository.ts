cceb64e3669dd89cc72a02ffbf9356f081b21745
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:40:07 -0300

    fix: eslint fix

--- src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -6,40 +6,42 @@ import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaAnswerAttachmentsRepository
-  implements AnswerAttachmentsRepository {
-  constructor(private prisma: PrismaService) { }
+  implements AnswerAttachmentsRepository
+{
+  constructor(private prisma: PrismaService) {}
 
   async createMany(attachments: AnswerAttachment[]): Promise<void> {
     if (attachments.length === 0) {
-      return;
+      return
     }
-    const data = PrismaAnswerAttachmentMapper.toPersistanceUpdateMany(attachments)
+    const data =
+      PrismaAnswerAttachmentMapper.toPersistanceUpdateMany(attachments)
 
     await this.prisma.attachment.updateMany(data)
   }
 
   async deleteMany(attachments: AnswerAttachment[]): Promise<void> {
     if (attachments.length === 0) {
-      return;
+      return
     }
 
-    const attachmentIds = attachments.map(attachment => {
+    const attachmentIds = attachments.map((attachment) => {
       return attachment.id.toString()
     })
 
     await this.prisma.attachment.deleteMany({
       where: {
         id: {
-          in: attachmentIds
-        }
-      }
+          in: attachmentIds,
+        },
+      },
     })
   }
 
   async deleteManyByAnswerId(answerId: string): Promise<void> {
     await this.prisma.attachment.deleteMany({
       where: {
-        id: answerId
+        id: answerId,
       },
     })
   }
@@ -47,8 +49,8 @@ export class PrismaAnswerAttachmentsRepository
   async findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]> {
     const answerAttachments = await this.prisma.attachment.findMany({
       where: {
-        answerId
-      }
+        answerId,
+      },
     })
 
     return answerAttachments.map(PrismaAnswerAttachmentMapper.toDomain)
