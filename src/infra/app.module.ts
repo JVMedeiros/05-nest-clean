2086566b1808b4945268051d2d8a145d4a1bad8d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:39:27 -0300

    fix: eslint fix

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { envSchema } from './env/env'
 import { EnvModule } from './env/env.module'
-import { EnvService } from './env/env.service'
 import { HttpModule } from './http/http.module'
 
 @Module({
@@ -14,7 +13,7 @@ import { HttpModule } from './http/http.module'
     }),
     AuthModule,
     HttpModule,
-    EnvModule
+    EnvModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
