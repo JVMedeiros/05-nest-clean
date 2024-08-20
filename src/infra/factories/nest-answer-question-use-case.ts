01e59348b7fbb0b946a36156cc905b1a1f382a89
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-08-19 22:44:19 -0300

    fix: eslint fix

--- src/infra/factories/nest-answer-question-use-case.ts
+++ src/infra/factories/nest-answer-question-use-case.ts
@@ -1,7 +1,5 @@
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
-import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
